88aa9b7c30732b370c0ece85f0e4753e244d4747
Vladyslav <kytsevolov@gmail.com>
2020-09-28 17:13:05 -0600

    styles daylistitem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
 
 const DayListItem = ({ name, spots, selected, setDay }) => {
+  const liStyle = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0,
+  });
+
   return (
-    <li onClick={() => setDay(name)}>
+    <li onClick={() => setDay(name)} className={liStyle}>
       <h2 className='text--regular'>{name}</h2>
       <h3 className='text--light'>{spots} spots remainig</h3>
     </li>

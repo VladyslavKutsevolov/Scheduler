9dbcd3302f123d6d3283450a85ae529f5117b1e9
Vladyslav <kytsevolov@gmail.com>
2020-09-29 14:21:31 -0600

    crated interviewer list component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import InterviewerListItem from './InterviewerListItem';
+
+import 'components/InterviewerList.scss'
+
+const InterviewerList = ({ interviewers, interviewer, setInterviewer }) => {
+  return (
+    <section className='interviewers'>
+      <h4 className='interviewers__header text--light'>Interviewer</h4>
+      <ul className='interviewers__list'>
+        {interviewers.map((teacher) => (
+            <InterviewerListItem {...teacher} setInterviewer={setInterviewer} selected={teacher.id === interviewer} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default InterviewerList

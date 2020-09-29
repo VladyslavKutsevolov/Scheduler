ac6bb65f0055acaabec693a1852b1fde4466b194
Vladyslav <kytsevolov@gmail.com>
2020-09-29 13:45:55 -0600

    created and styles interviewer list item

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,3 +47,5 @@ export default function Application(props) {
     </main>
   );
 }
+
+
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './InterviewerListItem.scss';
+
+const InterviewerListItem = ({
+  id,
+  name,
+  avatar,
+  selected,
+  setInterviewer,
+}) => {
+  const liStyle = classNames('interviewers__item', {
+    'interviewers__item--selected': selected,
+  });
+
+  return (
+    <li className={liStyle} onClick={() => setInterviewer(id)}>
+      <img className='interviewers__item-image' src={avatar} alt={name} />
+      {selected && name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;

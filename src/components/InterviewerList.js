fa1b9da17da93042e024d027da07ac5992e99cd2
Vladyslav <kytsevolov@gmail.com>
2020-09-29 15:01:04 -0600

    refactored

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,13 +3,17 @@ import InterviewerListItem from './InterviewerListItem';
 
 import 'components/InterviewerList.scss'
 
-const InterviewerList = ({ interviewers, interviewer, setInterviewer }) => {
+const InterviewerList = ({ interviewers, value, onChange }) => {
   return (
     <section className='interviewers'>
       <h4 className='interviewers__header text--light'>Interviewer</h4>
       <ul className='interviewers__list'>
         {interviewers.map((teacher) => (
-            <InterviewerListItem {...teacher} setInterviewer={() => setInterviewer(teacher.id)} selected={teacher.id === interviewer} />
+          <InterviewerListItem
+            {...teacher}
+            setInterviewer={() => onChange(teacher.id)}
+            selected={teacher.id === value}
+          />
         ))}
       </ul>
     </section>

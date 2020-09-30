c633165e6d33dae28e3df69a087c91f9b71b505a
Vladyslav <kytsevolov@gmail.com>
2020-09-30 15:27:04 -0600

    retrieve days data from server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,80 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DayList from './DayList';
 
 import 'components/Application.scss';
 import Appointment from './Appointment';
-
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
-
-const appointments = [
-  {
-    id: 1,
-    time: '12pm',
-  },
-  {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 1,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
-      },
-    },
-  },
-  {
-    id: 3,
-    time: '2pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 2,
-        name: 'Tori Malcolm',
-        avatar: 'https://i.imgur.com/Nmx0Qxo.png',
-      },
-    },
-  },
-  {
-    id: 4,
-    time: '3pm',
-  },
-  {
-    id: 3,
-    time: '4pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 2,
-        name: 'Tori Malcolm',
-        avatar: 'https://i.imgur.com/Nmx0Qxo.png',
-      },
-    },
-  },
-  {
-    id: 4,
-    time: '5pm',
-  },
-];
+import axios from 'axios';
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days').then((res) => {
+      setDays([...res.data]);
+    });
+  }, [days]);
 
   return (
     <main className='layout'>
@@ -96,9 +35,9 @@ export default function Application(props) {
         />
       </section>
       <section className='schedule'>
-        {appointments.map((appointment) => (
+        {/* {appointments.map((appointment) => (
           <Appointment key={appointment.id} {...appointment} />
-        ))}
+        ))} */}
       </section>
     </main>
   );

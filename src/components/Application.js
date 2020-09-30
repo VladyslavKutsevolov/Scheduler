f35c000b5985cdc9afdd5452d5d697189e6fdaa1
Vladyslav <kytsevolov@gmail.com>
2020-09-29 19:53:36 -0600

    render appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import DayList from './DayList';
 
 import 'components/Application.scss';
+import Appointment from './Appointment';
 
 const days = [
   {
@@ -18,6 +19,57 @@ const days = [
     id: 3,
     name: 'Wednesday',
     spots: 0,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 3,
+    time: '2pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 2,
+        name: 'Tori Malcolm',
+        avatar: 'https://i.imgur.com/Nmx0Qxo.png',
+      },
+    },
+  },
+  {
+    id: 4,
+    time: '3pm',
+  },
+  {
+    id: 3,
+    time: '4pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 2,
+        name: 'Tori Malcolm',
+        avatar: 'https://i.imgur.com/Nmx0Qxo.png',
+      },
+    },
+  },
+  {
+    id: 4,
+    time: '5pm',
   },
 ];
 
@@ -43,9 +95,11 @@ export default function Application(props) {
           alt='Lighthouse Labs'
         />
       </section>
-      <section className='schedule'></section>
+      <section className='schedule'>
+        {appointments.map((appointment) => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
+      </section>
     </main>
   );
 }
-
-

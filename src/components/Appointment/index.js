49a7ec9f7ec0be87263c63ab47c0bfef6977bb31
Vladyslav <kytsevolov@gmail.com>
2020-09-29 19:36:47 -0600

    render appointment component and added stories for it

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 import './styles.scss';
 
-const Appointment = () => {
-  return <article className='appointment'></article>;
+const Appointment = ({ id, time, interview }) => {
+  return (
+    <article className='appointment'>
+      <Header time={time} />
+      {interview ? (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 };
 
 export default Appointment;

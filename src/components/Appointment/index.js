dac7a565f17ca6e7c58faa1fc54267806dcc3c9c
Vladyslav <kytsevolov@gmail.com>
2020-10-06 14:42:27 -0600

    mock axios tests

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -69,7 +69,7 @@ const Appointment = ({
   };
 
   return (
-    <article className='appointment'>
+    <article className='appointment' data-testid='appointment'>
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === ERROR_SAVE && (
